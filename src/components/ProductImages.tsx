4cbc1f53d441a4bd907930e7973e8c7f45214a94
John Mason <87030324+simkidd@users.noreply.github.com>
2024-04-02 00:43:17 +0100

    chore: product image thumbnail

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,15 +1,22 @@
+"use client";
 import { Product } from "@/interfaces/product.interface";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const ProductImages: React.FC<{
   product: Product;
 }> = ({ product }) => {
+  const [selectedImage, setSelectedImage] = useState(product?.images?.[0]);
+
+  const handleSelected = (img: string) => {
+    setSelectedImage(img);
+  };
+
   return (
     <div className="border w-full">
       <div className="w-full h-[380px] p-4">
         <Image
-          src={product?.thumbnail}
+          src={selectedImage}
           alt=""
           width={300}
           height={300}
@@ -18,7 +25,11 @@ const ProductImages: React.FC<{
       </div>
       <div className="flex">
         {product?.images.map((img, i) => (
-          <div key={i} className="size-28">
+          <div
+            key={i}
+            className="size-28 "
+            onClick={() => handleSelected(img)}
+          >
             <Image
               src={img}
               alt=""
