f2738312e013a2dda07e14c8b886a97d6003182a
John Mason <87030324+simkidd@users.noreply.github.com>
2024-04-06 21:44:20 +0100

    chore: added login. signup and forgot password pages

---
+++ src/app/(auth)/account/login/page.tsx
@@ -0,0 +1,42 @@
+import LoginForm from "@/components/LoginForm";
+import { Metadata } from "next";
+import Link from "next/link";
+
+const pageTitle = "Log into an account";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: pageTitle,
+  },
+};
+
+const LoginPage = () => {
+  return (
+    <div className="w-full">
+      <h3 className="mb-6 text-center text-2xl font-semibold">Login</h3>
+      <LoginForm />
+      <div>
+        <p className="text-sm text-center mb-4">
+          Forgot password?{" "}
+          <Link
+            href="/account/forgot-password"
+            className="font-medium hover:underline"
+          >
+            Reset
+          </Link>
+        </p>
+        <p className="text-sm text-center">
+          Don’t have an account?{" "}
+          <Link
+            href="/account/register"
+            className="font-medium hover:underline"
+          >
+            Signup
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
