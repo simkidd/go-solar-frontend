86bf2b80ca0a1ee4082fbb8ca644f4b28faf1073
John Mason <87030324+simkidd@users.noreply.github.com>
2024-04-13 12:49:33 +0100

    chore: user registration

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,16 +1,21 @@
 "use client";
-import { LoginInput } from "@/interfaces/auth.interface";
+import { SignUpInput } from "@/interfaces/auth.interface";
+import { axiosInstance } from "@/lib/axios";
 import { Input } from "@nextui-org/react";
 import { Eye, EyeOff } from "lucide-react";
-import Link from "next/link";
-import { ChangeEvent, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
 
 const SignUpForm = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const [input, setInput] = useState<LoginInput>({
+  const [input, setInput] = useState<SignUpInput>({
     email: "",
     password: "",
+    fullname: "",
+    phonenumber: "",
   });
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const validateEmail = (input: string) =>
     input.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
@@ -21,28 +26,69 @@ const SignUpForm = () => {
     return validateEmail(input.email) ? false : true;
   }, [input.email]);
 
-  const validatePassword = (input: string) => input.length >= 5;
+  const validatePassword = (input: string) => input.length >= 6;
 
   const isPasswordInvalid = useMemo(() => {
     if (input.password === "") return false;
     return !validatePassword(input.password);
   }, [input.password]);
 
+  const isNameValid = useMemo(() => {
+    if (input.fullname === "") return false;
+  }, [input.fullname]);
+
+  const isPhoneValid = useMemo(() => {
+    if (input.phonenumber === "") return false;
+  }, [input.phonenumber]);
+
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!input.email || !input.password) {
+    if (
+      !input.email ||
+      !input.password ||
+      !input.fullname ||
+      !input.phonenumber
+    ) {
       alert("All fields are required");
       return;
     }
+    setLoading(true);
+    try {
+      const { data } = await axiosInstance.post("/auth/signup", input);
 
-    alert("Form submitted");
+      if (data.data.user) {
+        alert(data.message);
+        router.push("/account/login");
+      }
+    } catch (error) {
+      const errorMsg = error as any;
+      alert(errorMsg?.response.data.message);
+      console.log(errorMsg?.response.data.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit} className="mb-8">
+      <div className="input-group mb-3">
+        <Input
+          type="text"
+          variant="underlined"
+          label="Fullname"
+          size="lg"
+          className="w-full"
+          classNames={{
+            label: "text-black/50 dark:text-white/90",
+          }}
+          color={isNameValid ? "danger" : "success"}
+          value={input?.fullname}
+          onChange={(e) => setInput({ ...input, fullname: e.target.value })}
+        />
+      </div>
       <div className="input-group mb-3">
         <Input
           type="email"
@@ -90,14 +136,29 @@ const SignUpForm = () => {
           }
           color={isPasswordInvalid ? "danger" : "success"}
           errorMessage={
-            isPasswordInvalid && "Password must be at least 5 characters"
+            isPasswordInvalid && "Password must be at least 6 characters"
           }
           value={input?.password}
           onChange={(e) => setInput({ ...input, password: e.target.value })}
         />
       </div>
+      <div className="input-group mb-3">
+        <Input
+          type="text"
+          variant="underlined"
+          label="Phone"
+          size="lg"
+          className="w-full"
+          classNames={{
+            label: "text-black/50 dark:text-white/90",
+          }}
+          color={isPhoneValid ? "danger" : "success"}
+          value={input?.phonenumber}
+          onChange={(e) => setInput({ ...input, phonenumber: e.target.value })}
+        />
+      </div>
       <button className="w-full bg-primary text-white py-2 px-8 mt-8">
-        Sign Up
+        {loading ? "Loading..." : "Sign Up"}
       </button>
     </form>
   );
