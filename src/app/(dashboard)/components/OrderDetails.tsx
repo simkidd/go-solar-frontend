80c438d0a3480a214a570894a1ab061044d001da
John Mason <87030324+simkidd@users.noreply.github.com>
2024-05-14 00:15:53 +0100

    started order details page

---
+++ src/app/(dashboard)/components/OrderDetails.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { axiosInstance } from "@/lib/axios";
+import { useOrderStore } from "@/lib/stores/order.store";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const OrderDetails = () => {
+  const { order, setOrder } = useOrderStore();
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      if (id) {
+        try {
+          setLoading(true);
+          const { data } = await axiosInstance.get(`users/orders/${id}`);
+          setOrder(data.order);
+        } catch (error) {
+          const errorMsg = error as any;
+          console.log(errorMsg?.response.data.message);
+        } finally {
+          setLoading(false);
+        }
+      }
+    })();
+  }, [id]);
+
+  return (
+    <>
+      <div className="mb-8">
+        <h3 className="text-2xl font-medium">
+          Order ID: <span>#{order?.trackingId?.tracking_id}</span>
+        </h3>
+      </div>
+      <div className="w-full bg-white dark:bg-[#222327] py-16 px-6 shadow rounded">
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default OrderDetails;
